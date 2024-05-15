37d3660156adc366561329ad17c94027a55f85eb
Ankush Trakru <ankush.trakru@kroger.com>
2024-05-15 17:12:26 -0400

    check jfrog

--- project/app/jfrog.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://krogertechnology.jfrog.io/artifactory/api/repositories"
-headers = {
-    "X-JFrog-Art-Api": "AKCpBseMqpibWHcMmt6UfmrYuwdmW3U2DMPwVsXSwQmVykyWim8sk1J24wZvWPq8UQfbbvwwv"
-}
-
-response = requests.get(url, headers=headers)
-print(response.text)
